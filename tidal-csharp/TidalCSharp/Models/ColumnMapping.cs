7aae442daa3df22af58bfa01095c71e499140e56
rc38743874 <22777949+rc38743874@users.noreply.github.com>
2018-09-08 19:02:52 -0400

    Mapping files uncommitted

---
+++ tidal-csharp/TidalCSharp/Models/ColumnMapping.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace TidalCSharp {
+	public class ColumnMapping {
+		public string ColumnName { get; set; }
+		public string PropertyName { get; set; }
+		public bool ForceToBit { get; set; }
+	}
+}
---
+++ tidal-csharp/TidalCSharp/Models/TableMapping.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TidalCSharp {
+	public class TableMapping {
+		public string TableName { get; set; }
+		public string ObjectName { get; set; }
+
+		public ColumnMapping[] ColumnArray { get; set; }
+
+	}
+}
